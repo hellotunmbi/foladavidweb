08616382a24d242f44b76b8ddeaa28108e7ee0b0
olajide olamide <olajideseyi8181@gmail.com>
2024-06-27 22:37:47 +0100

    updated

--- src/components/MarqueeGroup.vue
+++ src/components/MarqueeGroup.vue
@@ -9,84 +9,86 @@
         Partners
       </h2>
     </div> -->
-    <div
-      class="marquee__group flex items-center gap-10 justify-around flex-shrink-0 min-w-full"
-    >
-      <div class="flex items-center h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808673/fmy_ow0hhn.png"
-          alt=""
-          class="h-20 w-30 object-cover"
-        />
+    <div class="marquee--reverse m-auto md:mt-8 flex gap-8 overflow-hidden">
+      <div
+        class="marquee__group flex items-center gap-10 justify-around flex-shrink-0 min-w-full"
+      >
+        <div class="flex items-center h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808673/fmy_ow0hhn.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808679/lys_bh26zl.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808680/nga_zawynp.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808684/ntdc_j0ry1p.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808687/ntn_ms3alr.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
       </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808679/lys_bh26zl.png"
-          alt=""
-          class="h-20 w-30 object-cover"
-        />
-      </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808680/nga_zawynp.png"
-          alt=""
-          class="h-20 w-30 object-cover"
-        />
-      </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808684/ntdc_j0ry1p.png"
-          alt=""
-          class="h-20 w-30 object-cover"
-        />
-      </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808687/ntn_ms3alr.png"
-          alt=""
-          class="h-20 w-30 object-cover"
-        />
-      </div>
-    </div>
 
-    <div
-      aria-hidden="true"
-      class="marquee__group flex items-center gap-10 justify-around flex-shrink-0 min-w-full"
-    >
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808673/fmy_ow0hhn.png"
-          alt=""
-          class="h-full w-full"
-        />
-      </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808679/lys_bh26zl.png"
-          alt=""
-          class="h-full w-full"
-        />
-      </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808680/nga_zawynp.png"
-          alt=""
-          class="h-full w-full"
-        />
-      </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808684/ntdc_j0ry1p.png"
-          alt=""
-          class="h-full w-full"
-        />
-      </div>
-      <div class="flex items-center gap-2 h-20 w-30">
-        <img
-          src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808687/ntn_ms3alr.png"
-          alt=""
-          class="h-full w-full"
-        />
+      <div
+        aria-hidden="true"
+        class="marquee__group flex items-center gap-10 justify-around flex-shrink-0 min-w-full"
+      >
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808673/fmy_ow0hhn.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808679/lys_bh26zl.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808680/nga_zawynp.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808684/ntdc_j0ry1p.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
+        <div class="flex items-center gap-2 h-20 w-30">
+          <img
+            src="https://res.cloudinary.com/dtkbcuiit/image/upload/v1718808687/ntn_ms3alr.png"
+            alt=""
+            class="h-20 w-30 object-cover"
+          />
+        </div>
       </div>
     </div>
   </div>
